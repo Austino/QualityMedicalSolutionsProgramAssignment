b74ca3dd8fdadac4358a1bafb3fce4a6b9f46500
James Austin <james.alan.austin@gmail.com>
2021-06-05 07:47:28 +0100

    Add project files.

---
+++ QualityMedicalSolutionsProgramAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QualityMedicalSolutionsProgramAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
